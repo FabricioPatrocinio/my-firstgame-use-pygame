dc5d69daba3bf7b6f15a855dbcb7bd86924befa7
fabriciopatrocinio <fabricioschiffer@gmail.com>
2021-03-16 14:58:25 -0300

    Animations ok, video 05 ok

--- first-game.py
+++ first-game.py
@@ -8,7 +8,7 @@ pygame.init()  # Inicia o game
 
 pygame.display.set_caption("Meu primeiro jogo")
 
-WINDOW_SIZE = (600, 400)
+WINDOW_SIZE = (750, 450)
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # Inicializa a janela
 
@@ -19,17 +19,8 @@ moving_left = False
 player_y_momentum = 0
 air_timer = 0
 
-player = pygame.image.load('assets/player.png')
-player.set_colorkey((255, 255, 255))
 
-player_rect = pygame.Rect(50, 50, player.get_width(), player.get_height())
-
-grass_img = pygame.image.load('assets/grass.png')
-TILE_SIZE = grass_img.get_width()
-
-dirt_img = pygame.image.load('assets/dirt.png')
-
-scroll = [0, 0]
+true_scroll = [0, 0]
 
 
 def load_map(path):
@@ -43,7 +34,55 @@ def load_map(path):
     return game_map
 
 
+global animation_frames
+animation_frames = {}
+
+
+def load_animation(path, frame_durations):  # [7,7] animação personagem
+    global animation_frames
+    animation_name = path.split('/')[-1]
+    animation_frame_data = []
+    n = 0
+    for frame in frame_durations:
+        animation_frame_id = animation_name + '_' + str(n)
+        img_loc = path + '/' + animation_frame_id + '.png'
+        animation_image = pygame.image.load(img_loc).convert()
+        animation_image.set_colorkey((255, 255, 255))
+        animation_frames[animation_frame_id] = animation_image.copy()
+        for i in range(frame):
+            animation_frame_data.append(animation_frame_id)
+        n += 1
+    return animation_frame_data
+
+
+def change_action(action_var, frame, new_value):
+    if action_var != new_value:
+        action_var = new_value
+        frame = 0
+    return action_var, frame
+
+
+animation_database = {}
+
+animation_database['run'] = load_animation('player_animations/run', [7, 7])
+animation_database['idle'] = load_animation(
+    'player_animations/idle', [7, 7, 40])
+
+player_action = 'idle'
+player_frame = 0
+player_flip = False
+
+player_rect = pygame.Rect(100,100,5,13)
+
 game_map = load_map('maps/map')
+
+grass_img = pygame.image.load('assets/grass.png')
+TILE_SIZE = grass_img.get_width()
+
+dirt_img = pygame.image.load('assets/dirt.png')
+
+background_objects = [[0.25, [120, 10, 70, 400]], [0.25, [280, 30, 40, 400]], [
+    0.5, [30, 40, 40, 400]], [0.5, [130, 90, 100, 400]], [0.5, [300, 80, 120, 400]]]
 
 
 def collision_test(rect, tiles):
@@ -81,8 +120,20 @@ def move(rect, movement, tiles):
 while True:  # Loop no game
     display.fill((146, 244, 255))
 
-    scroll[0] += (player_rect.x-scroll[0]-152)/20
-    scroll[1] += (player_rect.y-scroll[1]-106)/20
+    true_scroll[0] += (player_rect.x-true_scroll[0]-152)/20
+    true_scroll[1] += (player_rect.y-true_scroll[1]-106)/20
+    scroll = true_scroll.copy()
+    scroll[0] = int(scroll[0])
+    scroll[1] = int(scroll[1])
+
+    pygame.draw.rect(display, (7, 80, 75), pygame.Rect(0, 120, 300, 80))
+    for background_object in background_objects:
+        obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0], background_object[1]
+                               [1]-scroll[1]*background_object[0], background_object[1][2], background_object[1][3])
+        if background_object[0] == 0.5:
+            pygame.draw.rect(display, (14, 222, 150), obj_rect)
+        else:
+            pygame.draw.rect(display, (9, 91, 85), obj_rect)
 
     tile_rects = []
     y = 0
@@ -108,6 +159,18 @@ while True:  # Loop no game
     if player_y_momentum > 3:
         player_y_momentum = 3
 
+    if player_movement[0] > 0:  # Pega as ações
+        player_action, player_frame = change_action(
+            player_action, player_frame, 'run')
+        player_flip = False
+    if player_movement[0] == 0:
+        player_action, player_frame = change_action(
+            player_action, player_frame, 'idle')
+    if player_movement[0] < 0:
+        player_action, player_frame = change_action(
+            player_action, player_frame, 'run')
+        player_flip = True
+
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
 
     if collisions['bottom']:
@@ -116,14 +179,20 @@ while True:  # Loop no game
     else:
         air_timer += 1
 
-    display.blit(player, (player_rect.x-scroll[0], player_rect.y-scroll[1]))
+    player_frame += 1
+    if player_frame >= len(animation_database[player_action]):
+        player_frame = 0
+    player_img_id = animation_database[player_action][player_frame]
+    player_img = animation_frames[player_img_id]
+    display.blit(pygame.transform.flip(player_img, player_flip, False), (player_rect.x -
+                                                                         scroll[0], player_rect.y-scroll[1]))
 
     for event in pygame.event.get():  # Evento loop
         if event.type == QUIT:  # Checa o evento fechar da janela
             pygame.quit()  # Para o jogo
             sys.exit()  # Para o script
 
-        if event.type == KEYDOWN:
+        if event.type == KEYDOWN:  # Checa se foi pressionada uma tecla
             if event.key == K_RIGHT:
                 moving_right = True
             if event.key == K_LEFT:
